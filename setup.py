eeb6a2c11ff0e70cf5ffab4a5283aac5e712f364
gdassori <guido.dassori@gmail.com>
2018-03-08 19:58:49 +0100

    patch versioning

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_version():
     with open("connectrum/__init__.py") as f:
         for line in f:
             if line.startswith("__version__"):
-                return eval(line.split("=")[-1])
+                return eval(line.split("=")[-1]) + '-gdassori'
 
 REQUIREMENTS = [
     # none at this time
